bd46ef85dced61269c03ac5ec6b6b9df263930a7
yarduon <yard.campbs@gmail.com>
2024-05-01 19:35:55 +0100

    test3

--- main.js
+++ main.js
@@ -78,7 +78,7 @@ document.getElementById("confirm-answer").addEventListener("click", () => {
 });
 
 const qrScanner = new QrScanner(
-  document.getElementById("video"),
+  videoElem,
   result => console.log('decoded qr code:', result),
   { true: true },
   
