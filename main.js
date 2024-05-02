987dd0c47022d06540efe0c72ced72cca06aca3a
yarduon <yard.campbs@gmail.com>
2024-05-02 18:12:07 +0100

    test22

--- main.js
+++ main.js
@@ -89,6 +89,7 @@ document.getElementById("test").addEventListener("click", () => {
         }
       );
       qrScanner.start();
+      console.log("helou");
     })
     .catch((e) => {
       console.log(e);
